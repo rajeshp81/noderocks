437f1e78436a6bbc20dbf1eb1d3b8962cd1e8fc7
rajeshp81 <rajeshp81@gmail.com>
2019-04-19 23:01:04 -0700

    added New files

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/login', (req, res) => res.sendFile(__dirname+'/index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
